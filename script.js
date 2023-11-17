daee40e16418150b4bcae7dcb32eb677dfe4d820
Brandon <beegee54321@gmail.com>
2023-11-16 20:35:42 -0800

    added arrival departure calendar in script/html

--- script.js
+++ script.js
@@ -29,5 +29,33 @@ console.log(searchAirportEl);
 searchAirportEl.addEventListener("submit", checkAirports);
 
 $( function() {
-  $( "#datepicker" ).datepicker();
+  var dateFormat = "mm/dd/yy",
+    from = $( "#from" )
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1
+      })
+      .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+      }),
+    to = $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1
+    })
+    .on( "change", function() {
+      from.datepicker( "option", "maxDate", getDate( this ) );
+    });
+
+  function getDate( element ) {
+    var date;
+    try {
+      date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+      date = null;
+    }
+
+    return date;
+  }
 } );

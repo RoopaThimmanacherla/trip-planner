a304928c080b938139f05e22956ac7604c992fb0
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-16 20:50:45 -0800

    added checklist and updated styling

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ function checkAirports(event) {
 }
 console.log(searchAirportEl);
 searchAirportEl.addEventListener("submit", checkAirports);
+
+
+// Checklist functionality
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    return;
+  } else {
+    document.getElementById("checklist-items").appendChild(li);
+  }
+  document.getElementById("checklist-input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}

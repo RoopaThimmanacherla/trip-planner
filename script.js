5462a00c5e29bda76de27d888bcda092414ba9c9
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-17 20:52:08 -0800

    changed background image, updated checklist and search box styles and postitions, worked on checklist function

--- script.js
+++ script.js
@@ -145,14 +145,5 @@ function newElement() {
   document.getElementById("checklist-input").value = "";
 
   var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.appendChild(txt);
   li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
 }

89503eac4df48469f64a1d4de6169a3a68e8f193
Brandon <beegee54321@gmail.com>
2023-11-18 18:38:27 -0800

    stages files to switch branches

--- script.js
+++ script.js
@@ -59,3 +59,29 @@ $( function() {
     return date;
   }
 } );
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    return;
+  } else {
+    document.getElementById("checklist-items").appendChild(li);
+  }
+  document.getElementById("checklist-input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
